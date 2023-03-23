9278404dd730312916b7ccc60a3fec3e934bfc12
masayareb <masaya.1021.joho@icloud.com>
2023-03-23 23:33:55 +0900

    ユーザ管理機能・単体テスト実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'a000000'}
+    password_confirmation {password}
+    firstname {"あ"}
+    lastname {"あ"}
+    firstname_kana {"ア"} 
+    lastname_kana {"ア"}
+    date_of_birth {19900101}
+  end
+end
