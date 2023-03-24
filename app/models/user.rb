89a6236542af7764dc5b77e67f9e10b2ab440546
masayareb <masaya.1021.joho@icloud.com>
2023-03-24 20:46:12 +0900

    ユーザ管理機能・単体テスト実装_修正1
    正規表現の修正
    テストコード正常系の実装
    PWに関してのテストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Password is invalid. Include both letters and numbers" }
   validates :nickname, presence: true
-  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Firstname is invalid. Input full-width characters" }
-  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Lastname is invalid. Input full-width characters" }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Firstname is invalid. Input full-width characters" }
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Lastname is invalid. Input full-width characters" }
   validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "firstname_kana is invalid. Input full-width katakana characters" } 
   validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Lastname_kana is invalid. Input full-width katakana characters" }
   validates :date_of_birth, presence: true
