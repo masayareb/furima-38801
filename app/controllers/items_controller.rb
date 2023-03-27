a18164e2b4fd86ec0c24e05b7ff333eea7a6fbfa
masayareb <masaya.1021.joho@icloud.com>
2023-03-27 21:19:04 +0900

    商品出品機能実装
    JSの計算、単体テスト未実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,35 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, only: [:new,:create]
+
   def index
+    @item = Item.new
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if  @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item, :content, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
+  end
+
 end
+
---
+++ app/models/item.rb
@@ -0,0 +1,28 @@
+class Item < ApplicationRecord
+  #アソシエーション
+  belongs_to :user
+  has_one :order
+
+  #Active Storageのアソシエーション
+  has_one_attached :image
+
+  #ActiveHashのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions 
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  #バリデーション
+  validates :item, presence: true
+  validates :image, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+end
