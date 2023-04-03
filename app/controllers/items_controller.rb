8263980406d7a1596cdd9777cc39ecc25d9ed8da
masayareb <masaya.1021.joho@icloud.com>
2023-04-03 21:21:22 +0900

    商品情報編集機能実装_修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if @item.user_id != current_user.id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if  @item.update(item_params)
       redirect_to item_path(@item.id)
     else
